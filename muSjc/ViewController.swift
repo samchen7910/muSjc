8d8943b2873b3493b70a1878b8275f7905d82270
Tran Gia Huy <ghuy.dtt@gmail.com>
2020-04-21 00:19:41 +0200

    Initial project setup

---
+++ muSjc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  muSjc
+//
+//  Created by Tran Gia Huy on 4/21/20.
+//  Copyright © 2020 Tran Gia Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
